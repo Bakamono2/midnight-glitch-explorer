41fb918c1a926720502526b04cc395f0448dde85
Bakamono2 <littpenger@gmail.com>
2025-11-28 05:50:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -221,7 +221,7 @@ function App() {
           }}
           aria-label={isTimelineOpen ? 'Close timeline' : 'Open timeline'}
         >
-          {isTimelineOpen ? 'Right Arrow' : 'Left Arrow'}
+          {isTimelineOpen ? 'Right Arrow' : '►'}
         </button>
 
         <div style={{
